0812acf54e89ed01568552f540a269724821125f
anton-at-work <antonatwork@ya.ru>
2020-03-02 01:09:46 +0300

    Создание модуля открытия окна фото из галереи

---
+++ js/preview.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function () {
+
+  var photoPreview = document.querySelector('.big-picture');
+
+  photoPreview.classList.remove('hidden');
+  document.querySelector('body').classList.add('modal-open');
+
+})();
